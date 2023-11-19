129b08f593af554eb5ce34d2a27abf0fe4b1b40f
Luca Etienne Schulz <git@les.cx>
2023-11-19 18:31:11 +0100

    * Add extension icons
    * Add extension icons in multiple sizes
    * Improve code readability and stability

--- scripts/auto-clicker.js
+++ scripts/auto-clicker.js
@@ -1,29 +1,40 @@
 "use strict";
 
-if (typeof autoClickerInit === 'undefined') {
-  let autoClickerInterval;
-  let mousePosition = { x: 0, y: 0 };
-  const autoClickerInit = true; // Flag to prevent re-initialization
+// Mark the script as injected
+window.autoClickerInjected = true;
 
-  document.addEventListener('mousemove', (event) => {
-    mousePosition.x = event.clientX;
-    mousePosition.y = event.clientY;
-  });
+let isEnabled = false;
+let autoClickerInterval;
+let mousePosition = { x: 0, y: 0 };
 
-  function simulateClick() {
-    const element = document.elementFromPoint(mousePosition.x, mousePosition.y);
-    element?.click();
-  }
+document.addEventListener('mousemove', (event) => {
+  mousePosition.x = event.clientX;
+  mousePosition.y = event.clientY;
+});
 
-  function startAutoClicker() {
-    autoClickerInterval = setInterval(simulateClick, 1);
-  }
-
-  function stopAutoClicker() {
-    clearInterval(autoClickerInterval);
-  }
-
-  chrome.runtime.onMessage.addListener((message) => {
-    message.autoClickerEnabled ? startAutoClicker() : stopAutoClicker();
-  });
+// simulateClick() currently retrieves the cursor position once every
+// Millisecond. This is overkill when the mouse doesn't actually move.
+// There is a lot of performance saving potential here.
+function simulateClick() {
+  const element = document.elementFromPoint(mousePosition.x, mousePosition.y);
+  element?.click();
 }
+
+// There might be a better way to start and stop the auto clicker then by
+// clearing autoClickerInterval
+function startAutoClicker() {
+  autoClickerInterval = setInterval(simulateClick, 5);
+}
+
+function stopAutoClicker() {
+  clearInterval(autoClickerInterval);
+}
+
+// Event listener for messages from the service worker
+// to start or stop the auto clicker.
+chrome.runtime.onMessage.addListener((message) => {
+  if (message.toggle) {
+    isEnabled = !isEnabled;
+    isEnabled ? startAutoClicker() : stopAutoClicker();
+  }
+});
--- service-worker.js
+++ service-worker.js
@@ -1,42 +1,59 @@
 "use strict";
 
-let autoClickerEnabled = false;
-
+// getCurrentTabId() retrieves the ID of the currently active tab
+// in the last focused window,
+// which is necessary for sending messages to the correct tab.
 async function getCurrentTabId() {
-  let [tab] = await chrome.tabs.query({ active: true, lastFocusedWindow: true});
-  return tab.id;
+  try {
+    return (await chrome.tabs.query({ active: true, lastFocusedWindow: true }))[0]?.id;
+  } catch (err) {
+    console.error("error getting current tab ID: ", err);
+    return null;
+  }
 }
 
-async function toggleAutoClicker() {
+// This function injects the auto-clicker script into the specified tab
+// if it's not already injected.
+async function injectScriptIfNeeded(tabId) {
+  try {
+    // Check if the script is already injected
+    const [result] = await chrome.scripting.executeScript({
+      target: { tabId },
+      function: () => window.autoClickerInjected,
+    });
+
+    // If the script is not injected, inject it
+    if (!result?.result) {
+      await chrome.scripting.executeScript({
+        target: { tabId },
+        files: ['scripts/auto-clicker.js'],
+      });
+
+      // After successful injection, mark the script as injected
+      await chrome.scripting.executeScript({
+        target: { tabId },
+        function: () => { window.autoClickerInjected = true; },
+      });
+    }
+  } catch (err) {
+    console.error("Failed to inject or check script: ", err);
+  }
+}
+
+// This async function toggles the state of the extension.
+// It sends a message with { toggle: true } to the content script
+// running in the current tab. This approach simplifies the logic
+// by not requiring the service worker to keep track of the auto-clicker's state.
+async function toggleEnableExtension() {
   let currentTabId = await getCurrentTabId();
-  autoClickerEnabled = !autoClickerEnabled;
-  console.log("AutoClicker " + (autoClickerEnabled ? "enabled" : "disabled"));
-
-  if (autoClickerEnabled) {
-    chrome.scripting.executeScript({
-      target: { tabId: currentTabId },
-      files: ['scripts/auto-clicker.js'],
-    }).then(() => {
-      console.log("Injected scripts/auto-clicker.js");
-      chrome.tabs.sendMessage(currentTabId, { autoClickerEnabled });
-    }).catch(err => console.error("Script injection failed: ", err));
-  } else {
-    chrome.tabs.sendMessage(currentTabId, { autoClickerEnabled });
-  }
+  if (currentTabId) {
+    await injectScriptIfNeeded(currentTabId);
+    chrome.tabs.sendMessage(currentTabId, { toggle: true }).catch(err =>
+      console.error("failed to send message: ", err)
+    );
+  };
 }
 
-// Toggle auto clicker using commands
-chrome.commands.onCommand.addListener(async (command) => {
-  if (command === "toggle-auto-clicker") {
-    await toggleAutoClicker();
-  }
-});
-
-/*****************
-/  Context Menu  *
-*****************/
-
-// Create the parent item
 chrome.contextMenus.create({
   id: "autoClicker",
   title: "Auto Clicker",
@@ -50,18 +67,17 @@ chrome.contextMenus.create({
   contexts: ["all"]
 });
 
-//chrome.contextMenus.create({
-//  id: "followMouse",
-//  title: "Follow Mouse",
-//  parentId: "autoClicker",
-//  contexts: ["all"]
-//});
+// The listener for Chrome commands is set up to respond
+// to the "toggle-auto-clicker" command. When this command is triggered,
+// it calls toggleEnableExtension to toggle the state of the auto-clicker.
+chrome.commands.onCommand.addListener(async (command) => {
+  if (command === "toggle-auto-clicker") {
+    await toggleEnableExtension();
+  }
+});
 
-// Add click event listener
 chrome.contextMenus.onClicked.addListener(async (info, tab) => {
-  if (info.menuItemId === "specificLocation" || info.menuItemId === "followMouse") {
-    // Toggle Auto Clicker when either menu item is selected
-    await toggleAutoClicker();
-    // Additional logic for specific location or follow mouse can be added here
+  if (info.menuItemId === "specificLocation") {
+    await toggleEnableExtension();
   }
 });
